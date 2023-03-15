4e8d69d60c9d193755c4141066f66fe264cdb9f0
vpvm96 <vpvm96@gmail.com>
2023-03-15 18:23:22 +0900

    feat: Line Chart Data 추가

---
+++ src/utils/chartData.js
@@ -0,0 +1,11 @@
+export const data = [
+  { date: "Jan", value: 10 },
+  { date: "Feb", value: 50 },
+  { date: "Mar", value: 20 },
+  { date: "Apr", value: 40 },
+  { date: "May", value: 90 },
+  { date: "Jun", value: 100 },
+  { date: "Jul", value: 20 },
+  { date: "Aug", value: 30 },
+  { date: "Sep", value: 80 },
+]
