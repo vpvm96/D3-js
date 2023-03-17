fecb0ccc39b2e9d0c454f4ff97caa04eae0117af
vpvm96 <vpvm96@gmail.com>
2023-03-17 15:10:18 +0900

    fix: ScatterPlotChart 최종 구현 완료

--- src/components/scatterPlotChart/ScatterPlotChart.jsx
+++ src/components/scatterPlotChart/ScatterPlotChart.jsx
@@ -1,94 +1,53 @@
+import { useEffect } from "react"
 import * as d3 from "d3"
 
 const ScatterPlotChart = () => {
-  const svg = d3.select("div").attr("width", 1200).attr("height", 500)
+  useEffect(() => {
+    createPlotChart()
+  }, [])
 
-  const width = +svg.attr("width")
-  const height = +svg.attr("height")
+  const createPlotChart = () => {
+    const margin = { top: 10, right: 30, bottom: 30, left: 60 },
+      width = 460 - margin.left - margin.right,
+      height = 400 - margin.top - margin.bottom
 
-  const render = (data) => {
-    const xValue = (d) => d.horsepower
-    const xAxisLabel = "Horsepower"
-
-    const yValue = (d) => d.weight
-    const yAxisLabel = "Weight"
-
-    const margin = { top: 80, right: 40, bottom: 70, left: 150 }
-    const innerWidth = width - margin.left - margin.right
-    const innerHeight = height - margin.top - margin.bottom
-    const circleRadius = 10
-
-    const xScale = d3
-      .scaleLinear()
-      .domain(d3.extent(data, xValue))
-      .range([0, innerWidth])
-      .nice()
-
-    const yScale = d3
-      .scaleLinear()
-      .domain(d3.extent(data, yValue))
-      .range([0, innerHeight])
-      .nice()
-
-    const g = svg
+    const svg = d3
+      .select("body")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
       .append("g")
       .attr("transform", `translate(${margin.left}, ${margin.top})`)
 
-    const xAixs = d3.axisBottom(xScale).tickSize(-innerHeight).tickPadding(15)
-    const yAxis = d3.axisLeft(yScale).tickSize(-innerWidth).tickPadding(10)
+    d3.csv(
+      "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/2_TwoNum.csv"
+    ).then((data) => {
+      const x = d3.scaleLinear().domain([0, 4000]).range([0, width])
+      svg
+        .append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(x))
 
-    const yaixsG = g.append("g").call(yAxis)
+      const y = d3.scaleLinear().domain([0, 500000]).range([height, 0])
+      svg.append("g").call(d3.axisLeft(y))
 
-    yaixsG.selectAll(".domain").remove()
-    yaixsG
-      .append("text")
-      .attr("class", "axis-label")
-      .attr("y", -80)
-      .attr("x", -innerHeight / 2)
-      .attr("fill", "black")
-      .attr("transform", `rotate(-90)`)
-      .attr("text-anchor", "middle")
-      .text(yAxisLabel)
-
-    const xaixsG = g
-      .append("g")
-      .call(xAixs)
-      .attr("transform", `translate(0, ${innerHeight})`)
-
-    xaixsG.select(".domain").remove()
-    xaixsG
-      .append("text")
-      .attr("class", "axis-label")
-      .attr("y", 60)
-      .attr("x", innerWidth / 2)
-      .attr("fill", "black")
-      .text(xAxisLabel)
-
-    g.append("text").attr("class", "title").attr("y", -10).text("Cars")
-
-    g.selectAll("circle")
-      .data(data)
-      .enter()
-      .append("circle")
-      .attr("cy", (d) => yScale(yValue(d)))
-      .attr("cx", (d) => xScale(xValue(d)))
-      .attr("r", circleRadius)
+      svg
+        .append("g")
+        .selectAll("dot")
+        .data(data)
+        .join("circle")
+        .attr("cx", function (d) {
+          return x(d.GrLivArea)
+        })
+        .attr("cy", function (d) {
+          return y(d.SalePrice)
+        })
+        .attr("r", 1.5)
+        .attr("fill", "steelblue")
+    })
   }
 
-  d3.csv("https://vizhub.com/curran/datasets/auto-mpg.csv")
-    .then((data) => {
-      data.forEach((d) => {
-        d.map = +d.map
-        d.cylinders = +d.cylinders
-        d.displacement = +d.displacement
-        d.horsepower = +d.horsepower
-        d.weight = +d.weight
-        d.acceleration = +d.acceleration
-        d.year = +d.year
-      })
-      render(data)
-    })
-    .catch((error) => console.log(error))
+  return <></>
 }
 
 export default ScatterPlotChart
